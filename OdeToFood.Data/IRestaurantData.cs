5333c981e7bf5b63d31056f6e3d9b2b574ee4d1a
newmanuevers <seangnewman@msn.com>
2020-09-05 22:37:05 -0700

    Add project files.

---
+++ OdeToFood.Data/IRestaurantData.cs
@@ -0,0 +1,31 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public interface IRestaurantData
+    {
+        IEnumerable<Restaurant> GetAll();
+    }
+
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id= 1, Name ="Oregano's", Location = "Goodyear", Cuisine = CuisineType.Italian },
+                new Restaurant{Id= 2, Name ="Pei Wi", Location = "Avondale", Cuisine = CuisineType.Chinese },
+                new Restaurant{Id= 3, Name ="Honey Bear's", Location = "Phoenix", Cuisine = CuisineType.BBQ }
+
+            };
+        }
+        public IEnumerable<Restaurant> GetAll()
+        {
+            return from r in restaurants orderby r.Name select r;
+
+        }
+    }
+}
---
+++ OdeToFood/Pages/Restaurants/List.cshtml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using OdeToFood.Core;
+using OdeToFood.Data;
+using System.Collections.Generic;
+
+namespace OdeToFood
+{
+    public class ListModel : PageModel
+    {
+        private IConfiguration config;
+        private readonly IRestaurantData restaurantData;
+
+        public string Message { get; set; }
+        public IEnumerable<Restaurant>  Restaurants { get; set; }
+
+
+        public ListModel(IConfiguration config, IRestaurantData restaurantData)
+        {
+            this.config = config;
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet()
+        {
+            //Message = "Hello, World!";
+            Message = config["Message"];
+            Restaurants = restaurantData.GetAll();
+        }
+    }
+}
