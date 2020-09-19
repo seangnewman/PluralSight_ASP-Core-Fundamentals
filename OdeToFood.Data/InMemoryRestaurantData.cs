c2f05b98a35012909376e222c89a914f2b6ab7f3
newmanuevers <seangnewman@msn.com>
2020-09-18 23:41:39 -0700

    Implemented CRUD and changed from InMemory storage to  db storage.  Had issues using the local db but using SQL Server 2019 was fine.

---
+++ OdeToFood.Data/InMemoryRestaurantData.cs
@@ -0,0 +1,74 @@
+﻿using OdeToFood.Core;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdeToFood.Data
+{
+    public class InMemoryRestaurantData : IRestaurantData
+    {
+        List<Restaurant> restaurants;
+        public InMemoryRestaurantData()
+        {
+            restaurants = new List<Restaurant>()
+            {
+                new Restaurant{Id= 1, Name ="Oregano's", Location = "Goodyear", Cuisine = CuisineType.Italian },
+                new Restaurant{Id= 2, Name ="Pei Wi", Location = "Avondale", Cuisine = CuisineType.Chinese },
+                new Restaurant{Id= 3, Name ="Honey Bear's", Location = "Phoenix", Cuisine = CuisineType.BBQ }
+
+            };
+        }
+
+        public  Restaurant GetById(int id)
+        {
+            return restaurants.SingleOrDefault(r => r.Id == id);
+        }
+
+        public Restaurant  Add(Restaurant newRestaurant)
+        {
+            restaurants.Add(newRestaurant);
+            // for dev only
+            newRestaurant.Id = restaurants.Max(r => r.Id) + 1;
+            return newRestaurant;
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            var restaurant = restaurants.SingleOrDefault(r => r.Id == updatedRestaurant.Id);
+            if(restaurant != null)
+            {
+                restaurant.Name = updatedRestaurant.Name;
+                restaurant.Location = updatedRestaurant.Location;
+                restaurant.Cuisine = updatedRestaurant.Cuisine;
+            }
+
+            return restaurant;
+        }
+
+        public int Commit()
+        {
+            return 0;
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name=null)
+        {
+            return from r in restaurants
+                   orderby r.Name
+                   where string.IsNullOrEmpty(name) || r.Name.ToLower().StartsWith(name.ToLower())
+                   orderby r.Name
+                   select r;
+                   
+                   ;
+
+        }
+
+        public Restaurant Delete(int id)
+        {
+            var restaurant = restaurants.FirstOrDefault(r => r.Id == id);
+            if(restaurant != null)
+            {
+                restaurants.Remove(restaurant);
+            }
+            return restaurant;
+        }
+    }
+}
